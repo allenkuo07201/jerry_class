d235ae056b8eecdc3dc6d9d217213c0ce53e3c88
allen <allen@gmail.com>
2023-05-13 14:58:21 +0800

    修改樂透邏輯(數字不重複)

--- guess.py
+++ guess.py
@@ -1,6 +1,9 @@
 import random
-for i in range(6):
+numbers = set()
+while len(numbers) < 6:
+
     x = random.randint(1, 49)
-    print(x, end=' ')
-y = random.randint(1, 49)
-print(f"特別號：{y}")
+    numbers.add(x)
+
+
+print(numbers)
